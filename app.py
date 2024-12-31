eb5356245ef70354f8aa6aaa14198d4db7fb85d6
h-monishraj-1 <h.monishraj.official@gmail.com>
2025-01-01 04:30:31 +0530

    added game and movie recommender systems

--- app.py
+++ app.py
@@ -1,36 +1,18 @@
-# from flask import Flask, request, jsonify, render_template
-# from recommendation import MusicRecommender
-
-# app = Flask(__name__)
-
-# # Initialize the recommender system with your CSV file
-# recommender = MusicRecommender("ex.csv")
-
-# @app.route("/")
-# def home():
-#     return render_template("index.html")  # Render the frontend
-
-# @app.route("/recommend", methods=["POST"])
-# def recommend():
-#     data = request.json
-#     music_title = data.get("music_title", "")
-
-#     # Get recommendations
-#     recommendations = recommender.recommend(music_title)
-#     return jsonify({"recommendations": recommendations})
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
 from flask import Flask, request, jsonify, render_template
 from recommendation import MusicRecommender
 from book_recommendation import BookRecommender
+from movie_recommendation import MovieRecommender
+from games_recommendation import GameRecommender
+
 
 app = Flask(__name__)
 
 # Initialize both recommenders
 music_recommender = MusicRecommender("ex.csv")
 book_recommender = BookRecommender()
+movie_recommender = MovieRecommender("movies_list.pkl", "similarity.pkl")
+game_recommender = GameRecommender("game_data.pkl","games_similarity.pkl")
+
 
 @app.route("/")
 def home():
@@ -51,6 +33,33 @@ def recommend_book():
     book_title = data.get("book_title", "")
     recommendations = book_recommender.recommend(book_title)
     return jsonify({"recommendations": recommendations})
+
+
+@app.route("/recommend_movie", methods=["POST"])
+def recommend_movie():
+    data = request.json
+    movie_title = data.get("movie_title", "")
+    recommendations = movie_recommender.recommend(movie_title)
+    return jsonify({"recommendations": recommendations})
+
+
+
+# Game Recommendation Route
+@app.route("/recommend_game", methods=["POST"])
+def recommend_game():
+    data = request.json
+    game_name = data.get("game_name", "")
+    recommendations = []
+    if game_name:
+        try:
+            recommendations = game_recommender.recommend(game_name)
+        except ValueError as e:
+            return jsonify({"error": str(e)})
+    else:
+        return jsonify({"error": "No game name provided."})
+    return jsonify({"recommendations": recommendations})
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
