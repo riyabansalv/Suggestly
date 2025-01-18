b7e80949e73ff1dd0f8b284a79a1ed663d09dc67
h-monishraj-1 <h.monishraj.official@gmail.com>
2025-01-18 23:03:09 +0530

    updated movie

--- movie_recommendation.py
+++ movie_recommendation.py
@@ -7,7 +7,7 @@ class MovieRecommender:
         self.similarity = pickle.load(open(similarity_path, "rb"))
     
     def fetch_poster(self, movie_id):
-        url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=c7ec19ffdd3279641fb606d19ceb9bb1&language=en-US"
+        url = ""
         data = requests.get(url).json()
         poster_path = data.get('poster_path', '')
         return f"https://image.tmdb.org/t/p/w500/{poster_path}" if poster_path else ""
