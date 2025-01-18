01064a71e7720ea9530db3c240c3746ccb42bb40
h-monishraj-1 <h.monishraj.official@gmail.com>
2025-01-19 01:39:42 +0530

    added game routes to app.py

--- app.py
+++ app.py
@@ -12,11 +12,13 @@ app = Flask(__name__)
 music_recommender = MusicRecommender("music_list.pkl", "music_similarity.pkl")
 book_recommender = BookRecommender("book_list.pkl", "book_similarity.pkl")
 movie_recommender = MovieRecommender("movies_list.pkl", "movie_similarity.pkl")
+game_recommender = GameRecommender()
 
 # Load the music dataframe globally
 music_df = pd.read_pickle("music_list.pkl")
 books_df = pd.read_pickle("book_list.pkl")
 movies_df = pd.read_pickle("movies_list.pkl")
+games_df = pd.read_pickle("game_data.pkl")
 
 
 @app.route("/")
@@ -95,7 +97,27 @@ def get_titles():
 
 
 
+# game recommendation routes
+@app.route("/recommend_games", methods=["POST"])
+def recommend_games():
+    data = request.json
+    game_title = data.get("game_title", "")
+    recommendations = game_recommender.recommend(game_title)
+    # Adding images to the response
+    recommendation_details = [
+        {"title": game['name'], "image": game['image']} for game in recommendations
+    ]
 
+    return jsonify({"recommendations": recommendation_details})
+
+@app.route("/get_titles", methods=["GET"])
+def get_titles():
+    query = request.args.get("q", "").lower()
+    if not query:
+        return jsonify({"titles": []})
+    
+    matching_titles = games_df[games_df['Name'].str.lower().str.contains(query, na=False)]['Name'].head(10).tolist()
+    return jsonify({"titles": matching_titles})
 
 if __name__ == "__main__":
     app.run(debug=True)
